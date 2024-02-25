3a05cc5ba758aedab824eb1fd699375096f2acec
남수경 <nsg0909@naver.com>
2024-02-25 22:54:56 +0900

    otherpost.js 생성

---
+++ src/data/otherpost.js
@@ -0,0 +1,42 @@
+export default [
+  {
+    id: 0,
+    image: "https://cdn.imweb.me/thumbnail/20230522/31017371829ad.jpg",
+    title: "공릉역 카페 추천",
+    user: "제인로그",
+    date: 20240221,
+    likenum: 24,
+    commentnum: 1,
+    myfollowing: 1
+  },
+  {
+    id: 1,
+    image: "https://cdnimage.dailian.co.kr/news/202001/news_1578966048_861100_m_1.jpeg",
+    title: "돼지 등갈지 김치찜 만들기",
+    user: "김주부",
+    date: 20240219,
+    likenum: 110,
+    commentnum: 24,
+    myfollowing: 0
+  },
+  {
+    id: 2,
+    image: "https://cdnimage.dailian.co.kr/news/202001/news_1578966048_861100_m_1.jpeg",
+    title: "1박2일 국내여행 추천!",
+    user: "셩온더트립",
+    date: 20240201,
+    likenum: 1,
+    commentnum: 1,
+    myfollowing: 1
+  },
+  {
+    id: 3,
+    image: "https://cdnimage.dailian.co.kr/news/202001/news_1578966048_861100_m_1.jpeg",
+    title: "1월의 편의점 신상 털기",
+    user: "신상털이범",
+    date: 20240127,
+    likenum: 1,
+    commentnum: 1,
+    myfollowing: 0
+  }
+];
