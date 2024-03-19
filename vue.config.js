f215067adda7e755cd47ebb433a6a10347ab38b2
남수경 <nsg0909@naver.com>
2024-03-19 21:32:45 +0900

    outputDir docs로 바꾸고 다시 npm run build

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,6 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
+  outputDir: './docs',
   publicPath: '/portfolio-vue3-naverblog'
 })
